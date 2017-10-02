a90e7583254458df9cd04c73ef3d7d5af29a7ebc
Tim <tim.garret@inseec-france.com>
2017-10-02 12:09:09 +0200

    j'essaye tout seul

--- hello.rb
+++ hello.rb
@@ -1,1 +1,8 @@
 print "hello world"
+
+
+git status
+-> validation initiale fichiers non suivis
+git add hello.#!/usr/bin/env ruby -wKU
+git status
+-> nouveau fichier : hello.#!/usr/bin/env ruby -wKU
